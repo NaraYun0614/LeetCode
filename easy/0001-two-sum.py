0b9522f1a26df40a99b63d28ae35ea0aaec523e9
NaraYun0614 <nara.yun@mail.mcgill.ca>
2025-04-07 19:08:27 -0400

    April 7th 2025-01

---
+++ easy/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic ={}
+        for i, num in enumerate(nums):
+            comp = target - num
+            if comp in dic:
+                return [dic[comp],i]
+            dic[num] = i
+        
